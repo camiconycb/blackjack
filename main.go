318658a88d2f8678a8166ce073155ae5858afc8d
Camila <camicony.cb@gmail.com>
2025-04-14 08:20:16 -0400

    remove get token

--- main.go
+++ main.go
@@ -337,26 +337,13 @@ rule BlackjackNatural "Blackjack Natural" {
 `
 
 var extensionID = fmt.Sprintf("chrome-extension://%s", os.Getenv("extension_id"))
-var secretToken = os.Getenv("secret_token")
-
-func getTokenHandler(w http.ResponseWriter, r *http.Request) {
-	if secretToken == "" {
-		http.Error(w, "Server error: missing token", http.StatusInternalServerError)
-		return
-	}
-
-	response := map[string]string{"token": secretToken}
-	fmt.Println("response", response)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
-}
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 		fmt.Println("origin", origin)
-		fmt.Println("extensionID", extensionID)
 		if origin == extensionID {
+			fmt.Println("allow origin", origin)
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
@@ -368,29 +355,6 @@ func corsMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-func tokenMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if token != "Bearer "+secretToken {
-			fmt.Println("token ", token)
-			fmt.Println("secretToken ", secretToken)
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func extensionMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin != extensionID {
-			http.Error(w, "Forbidden: Only extension allowed", http.StatusForbidden)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	initRuleEngine()
@@ -398,8 +362,7 @@ func main() {
 	mux := http.NewServeMux()
 	handler := corsMiddleware(mux)
 
-	mux.Handle("/get-token", extensionMiddleware(http.HandlerFunc(getTokenHandler)))
-	mux.Handle("/api/advice", tokenMiddleware(http.HandlerFunc(adviceHandler)))
+	mux.Handle("/api/advice", http.HandlerFunc(adviceHandler))
 
 	port := os.Getenv("PORT")
 	if port == "" {
